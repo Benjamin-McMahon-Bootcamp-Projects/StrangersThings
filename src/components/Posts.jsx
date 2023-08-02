9b98b3f93bfed0c2106b486369600665998a4475
Benjamin McMahon <parachutehappens@gmail.com>
2023-08-02 09:31:44 -0700

    all posts fetch request built

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react'
+
+const mainBaseUrl = 'https://strangers-things.herokuapp.com/api/'
+const cohort = '2306-FTB-ET-WEB-FT'
+const baseUrl = mainBaseUrl + cohort
+
 export default function Posts() {
 
+  const [ allPosts, setAllPosts ] = useState([])
 
+  async function fetchAllPosts() {
+    try{
+
+      const response = await  fetch(`${baseUrl}/posts`)
+      const result = await response.json()
+      setAllPosts(result.data.posts)
+
+    }catch(err){
+      console.log('Error fetching all posts')
+    }
+  }
+  fetchAllPosts()
+
+  console.log(allPosts)
 
   return
   
